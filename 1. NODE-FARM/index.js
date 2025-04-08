9a413ab271f843dd85800a00cb85bde678ae520a
Vivek Negi <viveknegi230384@gmail.com>
2025-04-08 13:02:22 +0530

    lect 7

---
+++ 1. NODE-FARM/index.js
@@ -0,0 +1,6 @@
+const fs=require("fs"); //calling the fs module  "file system module"so that we can read,write etc from the file system  -> require(" ") ->this the the function used for calling the module in node js
+
+
+const hello="Hello World";
+console.log(hello);
+
