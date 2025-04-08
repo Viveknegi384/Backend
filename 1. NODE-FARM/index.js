8a923e59e8930d965c571dbeec80e9645b6872b3
Vivek Negi <viveknegi230384@gmail.com>
2025-04-08 14:48:18 +0530

    lect 8

--- 1. NODE-FARM/index.js
+++ 1. NODE-FARM/index.js
@@ -1,6 +1,16 @@
-const fs=require("fs"); //calling the fs module  "file system module"so that we can read,write etc from the file system  -> require(" ") ->this the the function used for calling the module in node js
+const { isUtf8 } = require("buffer");
+const fs=require("fs"); 
+//calling the fs module  "file system module"so that we can read,write etc from the file system  -> require(" ") ->this the the function used for calling the module in node js
 
 
-const hello="Hello World";
-console.log(hello);
+// const hello="Hello World";
+// console.log(hello);
 
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8'); 
+//fs.readFileSync(path, encoding): This synchronously reads the contents of the file at the specified path.'utf-8': Encoding format. Tells Node to return a string instead of a buffer.
+console.log(textIn)
+
+const textout= `This is what we know about avacado: ${textIn}.\n Created on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textout);
+//fs.writeFileSync(path, data):This synchronously writes the content of textout to './txt/output.txt'.If the file does not exist, it will be created.If it already exists, it will be overwritten.
+console.log("File Written!");
