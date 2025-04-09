ba3b8480374e26744f761930a69877586c8d3623
Vivek Negi <viveknegi230384@gmail.com>
2025-04-09 22:08:13 +0530

    lect 12

--- 1. NODE-FARM/index.js
+++ 1. NODE-FARM/index.js
@@ -1,5 +1,6 @@
 const http=require('http');
 const fs=require("fs"); 
+const url=require("url"); 
 //calling the fs module  "file system module"so that we can read,write etc from the file system  -> require(" ") ->this the the function used for calling the module in node js
 
 
@@ -42,7 +43,22 @@ console.log("Will read the file");
 /////////////////////////////////////////////////////////////////////////////
 //SERVER
 const server =http.createServer((req,res)=>{
-    res.end("Hello from the server!!");
+    //console.log(req.url);   //routing -> alg page pe jakar url change karna or "User kis page pe gaya hai, aur uske according hum kya response bhejenge."
+    const pathName=req.url;
+    if(pathName==='/' || pathName==='/overview') {
+        res.end("This is the overview");
+    }
+    else if(pathName==='/product'){
+     res.end("This is the product");
+    }
+    else{
+        res.writeHead(404,{
+            'Content-type' : 'text/html',
+            'my-own-header': 'Hello World'
+        })
+     res.end("<h1>Page not found</h1>");
+    }
+    
 });
 
 server.listen(8000,'127.0.0.1',()=>{
