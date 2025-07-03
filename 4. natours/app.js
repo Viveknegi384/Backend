e1ba26b6d42def0c1a143638efaa4a253e499afa
Vivek Negi <viveknegi230384@gmail.com>
2025-07-03 23:15:07 +0530

    56. Handling Delete Requests

--- 4. natours/app.js
+++ 4. natours/app.js
@@ -82,6 +82,7 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+//USE PATCH request
 app.patch('/api/v1/tours/:id', (req, res) => {
   if (req.params.id > tours.length) {
     return res.status(404).json({
@@ -95,6 +96,22 @@ app.patch('/api/v1/tours/:id', (req, res) => {
     data: {
       tour: '<Updated tour here...>',
     },
+  });
+});
+
+//USE DELETE request
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    //204 means "no content"
+    status: 'success',
+    data: null,
   });
 });
 
