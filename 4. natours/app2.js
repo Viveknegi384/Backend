bd4f7e3e5ee295f31dbf546b66a1c9f4c1e84d6f
Vivek Negi <viveknegi230384@gmail.com>
2025-07-05 17:31:15 +0530

    59. creating our own middleware

--- 4. natours/app2.js
+++ 4. natours/app2.js
@@ -4,13 +4,31 @@ const app = express();
 
 app.use(express.json()); //middleware
 
+//creating own middleware
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 👋');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+/*
+Yeh line request object (req) me ek naya custom property add karti hai → requestTime
+Isme current time store hota hai (ISO format me)
+Jaise: "2025-07-05T12:45:00.000Z"
+*/
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 const getAllTour = (req, res) => {
+  console.log(req.requestTime);  //Middleware ne req.requestTime me jo time store kiya tha, wo print karega.
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,  // exact time jab request aayi thi
     results: tours.length,
     data: {
       tours,
@@ -96,8 +114,12 @@ const deleteTour = (req, res) => {
 // app.delete('/api/v1/tours/:id', deleteTour);
 
 //using .route method common url ko likh diya and phir chaining kar di
-app.route("/api/v1/tours").get(getAllTour).post(createTour);
-app.route("/api/v1/tours/:id").get(getTour).patch(updateTour).delete(deleteTour);
+app.route('/api/v1/tours').get(getAllTour).post(createTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 const port = 3000;
 app.listen(port, () => {
