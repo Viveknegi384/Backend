3ac790f1db3a32ea2f6e92a9a430da7910d5a7a0
Vivek Negi <viveknegi230384@gmail.com>
2025-07-13 02:37:02 +0530

    67. setting environment variables 68. ESLint

--- 4. natours/app3.js
+++ 4. natours/app3.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
-
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
-app.use(morgan('dev'));
+if(process.env.NODE_ENV === 'devlopment'){
+    app.use(morgan('dev'));
+}
 
 app.use(express.json());
 //serving static file
 app.use(express.static(`${__dirname}/public`));//isme jo file specify kiya h hamne uss file li kisi bhi file ko directly url pe publish karna without going thoughout the routes done by this method
- 
+
 
 app.use((req, res, next) => {
     console.log('Hello from the middleware 👋');
--- 4. natours/server.js
+++ 4. natours/server.js
@@ -1,7 +1,11 @@
-  const app =require("./app3")
+const dotenv=require('dotenv')
+dotenv.config({path: "./config.env"});
+const app =require("./app3");
 
 
-const port = 3000;
+console.log(process.env);
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
