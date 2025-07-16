e28b793b0cff03108eb0ed61dd6ac6012c42b08c
Vivek Negi <viveknegi230384@gmail.com>
2025-07-17 01:09:49 +0530

    83. connecting db with the express app , 84. What is mongoose

--- 4. natours/server.js
+++ 4. natours/server.js
@@ -1,9 +1,21 @@
-const dotenv=require('dotenv')
-dotenv.config({path: "./config.env"});
-const app =require("./app3");
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const app = require("./app3");
+
+dotenv.config({ path: "./config.env" });
 
 
-console.log(process.env);
+// console.log(process.env);
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+
+//mongoose.connect(process.env.DATABASE_LOCAL, {  // if using local database
+mongoose.connect(DB, { //if using cloud database
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    //console.log(con.connections);
+    console.log('DB Connection sucessful!');
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
