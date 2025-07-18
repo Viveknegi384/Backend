95bed5028e21296147ced8ece1dcb6c17cc14a59
Vivek Negi <viveknegi230384@gmail.com>
2025-07-18 09:52:38 +0530

    85.creating a simple model tour

--- 4. natours/server.js
+++ 4. natours/server.js
@@ -17,6 +17,23 @@ mongoose.connect(DB, { //if using cloud database
     console.log('DB Connection sucessful!');
 })
 
+const tourSchema= new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,'A tour must have a name'],
+        unique:true
+    },
+    rating:{
+        type: Number,
+        default: 4.5
+    },
+    price:{
+        type:Number,
+        required:[true,'A tour must have a price']
+    }
+});
+const Tour = mongoose.model('Tour',tourSchema);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
