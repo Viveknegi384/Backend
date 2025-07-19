ff15e6d63fd1efc6e74aeeb72e9614987c2164eb
Vivek Negi <viveknegi230384@gmail.com>
2025-07-19 20:41:04 +0530

    86 Creating documents and testing the model

--- 4. natours/server.js
+++ 4. natours/server.js
@@ -17,22 +17,33 @@ mongoose.connect(DB, { //if using cloud database
     console.log('DB Connection sucessful!');
 })
 
-const tourSchema= new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true,'A tour must have a name'],
-        unique:true
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true
     },
-    rating:{
+    rating: {
         type: Number,
         default: 4.5
     },
-    price:{
-        type:Number,
-        required:[true,'A tour must have a price']
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price']
     }
 });
-const Tour = mongoose.model('Tour',tourSchema);
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+    name: "The Park Camper",
+    price: 997
+});
+
+testTour.save().then(doc => {
+    console.log(doc);
+}).catch(err => {
+    console.log('Error 🎆: ', err)
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
