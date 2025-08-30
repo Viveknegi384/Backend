cc4ac1322cab6592fcaf956fb2317b909e94e055
Vivek Negi <viveknegi230384@gmail.com>
2025-08-30 18:27:17 +0530

    122. Errors Outside Express Unhandled Rejection

--- 9. natours/server.js
+++ 9. natours/server.js
@@ -4,23 +4,29 @@ const dotenv = require('dotenv')
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
-
+ 
 
 // console.log(process.env);
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
 
 //mongoose.connect(process.env.DATABASE_LOCAL, {  // if using local database
-mongoose.connect(DB, { //if using cloud database
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    //console.log(con.connections);
-    console.log('DB Connection sucessful!');
-})
+mongoose.connect(DB)
+    .then(() => {
+        console.log('DB Connection successful!');
+    })
+    // .catch(err => console.error(err));
+
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
 
+process.on('unhandledRejection',err =>{
+    console.log(err.name,err.message);
+    console.log("UNHANDLER REJECTION! 💥 Shutting down...");
+    server.close(()=>{
+        process.exit(1);
+    })
+});
