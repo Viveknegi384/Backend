9a57cf55bcab4b2fe501a3237a98f5bc9357b9d9
Vivek Negi <viveknegi230384@gmail.com>
2025-10-05 21:15:05 +0530

    130. Logging in User

--- 10. natours/controllers/authController.js
+++ 10. natours/controllers/authController.js
@@ -1,6 +1,13 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const appError = require('../utils/appError');
+
+const signToken = id => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    });
+}
 
 exports.signup = catchAsync(async (req, res, next) => {
     // const newUser = await User.create(req.body); // This is not safe because user can add role as admin in req.body
@@ -11,9 +18,7 @@ exports.signup = catchAsync(async (req, res, next) => {
         passwordConfirm: req.body.passwordConfirm
     });
 
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN
-    });
+    const token = signToken(newUser._id);
 
     res.status(201).json({
         status: 'success',
@@ -23,3 +28,26 @@ exports.signup = catchAsync(async (req, res, next) => {
         }
     });
 });
+
+exports.login = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    //1) Check if email and password exist
+    if (!email || !password) {
+        return next(new appError('Please provide email and password!', 400));
+    }
+    //2) Check if user exists && password is correct
+    const user = await User.findOne({ email }).select('+password'); //as password field is set to select false in user schema so we have to explicitly select it here
+
+    //test1234 === $2b$12$EaA3njqA9wBNFbhI.5aukubGGHrK6B2JX5CUNquN0XpgmUcMR2oWi
+    if(!user || !(await user.correctPassword(password, user.password))){
+        return next(new appError('Incorrect email or password', 401));
+    }
+
+    //3) If everything ok, send token to client
+    const token = signToken(user._id);
+    res.status(200).json({
+        status: 'success',
+        token
+    });
+}
