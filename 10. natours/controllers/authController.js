02fe910b07427a9a32fe9431b4ad01012cf9f21d
Vivek Negi <viveknegi230384@gmail.com>
2025-10-22 16:21:13 +0530

    refactor: rename variable for clarity in protect middleware

--- 10. natours/controllers/authController.js
+++ 10. natours/controllers/authController.js
@@ -71,18 +71,18 @@ exports.protect = catchAsync(async (req, res, next) => {
     // console.log(decoded);     
 
     //3) Check if user still exists
-    const freshUser = await User.findById(decoded.id);
-    if (!freshUser) {
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
         return next(new appError('The user belonging to this token does no longer exist.', 401));
     }
 
     // 4) check if user changed password after the token was issued
-     if(freshUser.changedPasswordAfter(decoded.iat)) {
+     if(currentUser.changedPasswordAfter(decoded.iat)) {
          return next(new appError('User recently changed password! Please log in again.', 401));
      }
 
 
     //GRANT ACCESS TO PROTECTED ROUTE
-    req.user = freshUser;
+    req.user = currentUser;
     next();
 });
