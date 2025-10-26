3df0507e918ebe12dd8975f026e53a62e87fe4a6
Vivek Negi <viveknegi230384@gmail.com>
2025-10-26 18:12:39 +0530

    135,136,137. feat: integrate nodemailer for email functionality
    - Added StreamTransport class for streaming email messages.
    - Implemented well-known SMTP service configurations.
    - Created services.json for various email providers.
    - Developed XOAuth2 class for generating OAuth2 tokens.
    - Updated package.json to include nodemailer as a dependency.
    - Added email utility function to send emails using nodemailer.
    - Enhanced user routes with forgot and reset password functionalities.

--- 10. natours/models/userModel.js
+++ 10. natours/models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcrypt');
@@ -37,7 +38,9 @@ const userSchema = new mongoose.Schema({
             message: 'Passwords are not the same!'
         }
     },
-    passwordChangedAt: Date
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
 });
 
 userSchema.pre('save', async function (next) {
@@ -49,6 +52,13 @@ userSchema.pre('save', async function (next) {
 
     //Delete passwordConfirm field
     this.passwordConfirm = undefined;
+    next();
+});
+
+userSchema.pre('save', function (next) {
+    if (!this.isModified('password') || this.isNew) return next();
+
+    this.passwordChangedAt = Date.now() - 1000;
     next();
 });
 
@@ -67,6 +77,20 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
     //False means NOT changed
     return false;
 }
+
+userSchema.methods.createPasswordResetToken = function () {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+
+    crypto.createHash('sha256').update(resetToken).digest('hex');
+    this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+
+    console.log({resetToken}, this.passwordResetToken);
+
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000; //10 minutes
+
+    return resetToken;
+};
+
 
 const User = mongoose.model('User', userSchema);
 
