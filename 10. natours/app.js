62e374e8fd8571f00f9c476d52d5c3d5bc089a13
Vivek Negi <viveknegi230384@gmail.com>
2025-12-23 23:29:44 +0530

    143. Implementing Rate Limiting

--- 10. natours/app.js
+++ 10. natours/app.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const morgan = require('morgan');
-const AppError= require('./utils/appError');
+const rateLimit = require('express-rate-limit');
+
+
+const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
-if(process.env.NODE_ENV === 'devlopment'){
+//1) GLOBAL MIDDLEWARES
+if (process.env.NODE_ENV === 'devlopment') {
     app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+    max: 100, //max requests from same IP
+    windowMs: 60 * 60 * 1000, //1 hour
+    message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter); //to apply this limiter only on routes which start with /api
 
 app.use(express.json());
 //serving static file
@@ -31,12 +42,12 @@ app.use('/api/v1/users', userRouter);
 //here above tourRouter and userRouter are the two middleware which are then mount through app.use
 
 
-app.all('*', (req,res,next)=>{
+app.all('*', (req, res, next) => {
     // res.status(404).json({
     //     status: 'fail',
     //     message: `Cant't find ${req.originalUrl} on this server!`
     // })
-    
+
     /*
     const err= new Error(`Can't find ${req.originalUrl} on this server!`);
     err.status= 'fail';
@@ -45,7 +56,7 @@ app.all('*', (req,res,next)=>{
     next(err);
     */
 
-   next(new AppError(`Can't find ${req.originalUrl} on this server!`,404));
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 app.use(globalErrorHandler);
