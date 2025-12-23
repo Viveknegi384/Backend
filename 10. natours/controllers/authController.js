a72877cf0d0aecc05273880ac65e02abf48a108a
Vivek Negi <viveknegi230384@gmail.com>
2025-12-23 14:35:59 +0530

    138,139 updating current user password and current user data

--- 10. natours/controllers/authController.js
+++ 10. natours/controllers/authController.js
@@ -12,6 +12,17 @@ const signToken = id => {
     });
 }
 
+const createSendToken = (user, statusCode, res) => {
+    const token = signToken(user._id);  
+    res.status(statusCode).json({
+        status: 'success',
+        token,  
+        data: {
+            user
+        }
+    });
+}
+
 exports.signup = catchAsync(async (req, res, next) => {
     // const newUser = await User.create(req.body); // This is not safe because user can add role as admin in req.body
     const newUser = await User.create({
@@ -23,15 +34,8 @@ exports.signup = catchAsync(async (req, res, next) => {
         role: req.body.role
     });
 
-    const token = signToken(newUser._id);
-
-    res.status(201).json({
-        status: 'success',
-        token,
-        data: {
-            user: newUser
-        }
-    });
+    createSendToken(newUser, 201, res);
+    
 });
 
 exports.login = async (req, res, next) => {
@@ -50,11 +54,8 @@ exports.login = async (req, res, next) => {
     }
 
     //3) If everything ok, send token to client
-    const token = signToken(user._id);
-    res.status(200).json({
-        status: 'success',
-        token
-    });
+    createSendToken(user, 200, res);
+
 }
 
 exports.protect = catchAsync(async (req, res, next) => {
@@ -160,10 +161,25 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
 
 
     //4) LOG THE USER IN, SEND JWT
-    const token = signToken(user._id);
-    res.status(200).json({
-        status: 'success',
-        token
-    });
+    createSendToken(user, 200, res);
 
 });
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+    //1) Get user from collection
+    const user = await User.findById(req.user.id).select('+password');
+
+    //2) Check if POSTed current password is correct
+    if (!(await user.correctPassword(req.body.passwordCurrentf, user.password))) {
+        return next(new appError('Your current password is wrong.', 401));
+    }
+    //3) If so, update password
+    user.password = req.body.password;
+    user.passwordConfirm = req.body.passwordConfirm;
+    await user.save();
+    // User.findByIdAndUpdate will NOT work as intended!
+    
+
+    //4) Log user in, send JWT
+    createSendToken(user, 200, res);
+})
