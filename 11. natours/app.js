7c505b14222a29f80ea87ba86cbcf6bd57693888
Vivek Negi <viveknegi230384@gmail.com>
2025-12-25 21:35:40 +0530

    Starting new Section 11- Modelling Data and Advanced Mongoose

---
+++ 11. natours/app.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController')
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
+
+const app = express();
+
+//1) GLOBAL MIDDLEWARES
+//set security http headers
+app.use(helmet());//set security http headers
+
+//development logging
+if (process.env.NODE_ENV === 'devlopment') {
+    app.use(morgan('dev'));
+}
+
+//limit requests from same API
+const limiter = rateLimit({
+    max: 100, //max requests from same IP
+    windowMs: 60 * 60 * 1000, //1 hour
+    message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter); //to apply this limiter only on routes which start with /api
+
+//body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' })); //body size is limited to 10kb
+
+//Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+//Data sanitization against XSS attacks
+app.use(xss());
+
+//Prevent parameter pollution
+app.use(hpp({
+    whitelist: ['duration', 'ratingsQuantity', 'ratingsAverage', 'maxGroupSize', 'difficulty', 'price']
+}
+));
+
+//serving static file
+app.use(express.static(`${__dirname}/public`));//isme jo file specify kiya h hamne uss file li kisi bhi file ko directly url pe publish karna without going thoughout the routes done by this method
+
+//test middleware
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    // console.log(x); //error in postman
+    // console.log(req.headers);
+    next();
+});
+
+// console.log(x); //error in console handle by uncaught exception
+
+
+app.use('/api/v1/tours', tourRouter); //for this routes we want to apply this tourRouter middleware
+app.use('/api/v1/users', userRouter);
+//here above tourRouter and userRouter are the two middleware which are then mount through app.use
+
+
+app.all('*', (req, res, next) => {
+    // res.status(404).json({
+    //     status: 'fail',
+    //     message: `Cant't find ${req.originalUrl} on this server!`
+    // })
+
+    /*
+    const err= new Error(`Can't find ${req.originalUrl} on this server!`);
+    err.status= 'fail';
+    err.statusCode =404;
+
+    next(err);
+    */
+
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
---
+++ 11. natours/controllers/tourController.js
@@ -0,0 +1,192 @@
+//in this we handling of req will be done , saare exports.___ called as handlerfunction
+
+//const fs = require('fs');
+const Tour = require('../models/tourModel');
+const APIfeatures = require('../utils/apiFeatures');
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync')
+
+exports.aliasTopTour = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
+
+exports.getAllTour = catchAsync(async (req, res, next) => {
+
+  //EXECUTE QUERY
+  const features = new APIfeatures(Tour.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const tours = await features.query;
+
+
+  //SEND RESPONSE
+  res.status(200).json({
+    status: 'success',
+    //requestedAt: req.requestTime, // exact time jab request aayi thi
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+
+});
+
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findById(req.params.id);
+
+  if (!tour) { //as NULL value ko false ki hi tarah consider karte h 
+    return next(new AppError('No tour found with ID', 404));
+  }
+
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+
+});
+
+
+
+exports.createTour = catchAsync(async (req, res, next) => {
+  const newTour = await Tour.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      tours: newTour,
+    },
+  });
+
+  /*
+  try {
+    
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+  */
+});
+
+exports.updateTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!tour) { //as NULL value ko false ki hi tarah consider karte h 
+    return next(new AppError('No tour found with ID', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+
+});
+
+exports.deleteTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!tour) { //as NULL value ko false ki hi tarah consider karte h 
+    return next(new AppError('No tour found with ID', 404));
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+
+});
+
+exports.getTourStats = catchAsync(async (req, res, next) => {
+  const stats = await Tour.aggregate([
+    {
+      $match: { ratingsAverage: { $gte: 4.5 } }
+    },
+    {
+      $group: {
+
+        // _id:'$ratingsAverage',
+        _id: { $toUpper: '$difficulty' },
+        numTours: { $sum: 1 },
+        numRatings: { $sum: '$ratingsQuantity' },
+        avgRating: { $avg: '$ratingsAverage' },
+        avgPrice: { $avg: '$price' },
+        minPrice: { $min: '$price' },
+        maxPrice: { $max: '$price' }
+      }
+    },
+    {
+      $sort: { avgPrice: 1 }
+    },
+  ]);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      stats
+    },
+  });
+});
+
+exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
+
+  const year = req.params.year * 1;
+
+  const plan = await Tour.aggregate([
+    {
+      $unwind: '$startDates'
+    },
+    {
+      $match: {
+        startDates: {
+          $gte: new Date(`${year}-01-01`),
+          $lte: new Date(`${year}-12-31`),
+        }
+      }
+    },
+    {
+      $group: {
+        _id: { $month: '$startDates' },
+        numTourStarts: { $sum: 1 },
+        tours: { $push: '$name' }
+      }
+    },
+    {
+      $addFields: { month: '$id' }
+    },
+    {
+      $project: {
+        _id: 0
+      }
+    },
+    {
+      $sort: { numTourStarts: -1 }
+    },
+    {
+      $limit: 12
+    }
+  ]);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      plan
+    }
+  })
+
+
+});
