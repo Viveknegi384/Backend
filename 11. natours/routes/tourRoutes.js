6f71a74deea1e980209251a477df9e2718f169b7
Vivek Negi <viveknegi230384@gmail.com>
2026-01-03 23:15:26 +0530

    159. Nested Routes with Express

--- 11. natours/routes/tourRoutes.js
+++ 11. natours/routes/tourRoutes.js
@@ -2,8 +2,13 @@ const express = require("express")
 const tourController = require("../controllers/tourController") //import karna
 //instead of above we can also do that by desructuring
 const authController = require("../controllers/authController")
-const reviewController = require("../controllers/reviewController")
+// const reviewController = require("../controllers/reviewController")
+const reviewRouter = require('./reviewRoutes');
+
 const router = express.Router();
+
+
+
 
 // router.param('id', (req, res, next, val) => { //new par. val is added
 //     console.log(`Tour id is: ${val}`); //value is used to store id value
@@ -18,6 +23,15 @@ Create a checkBody middleware (means check karsakte h uss POST req ko api or db 
 If not,send back 400 (bad request)
 Add it to the post handler stack
 */
+
+
+// router.route('/:tourId/reviews').post(
+//     authController.protect,
+//     authController.restrictTo('user'),
+//     reviewController.createReview
+// );
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router.route('/top-5-cheap').get(tourController.aliasTopTour, tourController.getAllTour)
 
@@ -36,10 +50,6 @@ router
     .delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour);
 
 
-router.route('/:tourId/reviews').post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview
-);
+
 
 module.exports = router;
