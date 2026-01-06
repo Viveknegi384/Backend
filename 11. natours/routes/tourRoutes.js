65a72508b888a6d7c7f133e8b85e297136c5481d
Vivek Negi <viveknegi230384@gmail.com>
2026-01-06 14:16:23 +0530

    165.  Adding Missing Authentication and Authorization

--- 11. natours/routes/tourRoutes.js
+++ 11. natours/routes/tourRoutes.js
@@ -31,22 +31,22 @@ Add it to the post handler stack
 //     reviewController.createReview
 // );
 
-router.use('/:tourId/reviews', reviewRouter);
+router.use('/:tourId/reviews', reviewRouter); 
 
 router.route('/top-5-cheap').get(tourController.aliasTopTour, tourController.getAllTour)
 
 router.route('/tour-stats').get(tourController.getTourStats)
-router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan)
+router.route('/monthly-plan/:year').get(authController.protect, authController.restrictTo('admin', 'lead-guide',"guides"),tourController.getMonthlyPlan)
 
 router
     .route('/')
-    .get(authController.protect, tourController.getAllTour)
-    .post(tourController.createTour); //here we have implemented two middleware
+    .get(tourController.getAllTour)
+    .post(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.createTour); 
 
 router
     .route('/:id')
     .get(tourController.getTour)
-    .patch(tourController.updateTour)
+    .patch(authController.protect, authController.restrictTo('admin', 'lead-guide'),tourController.updateTour)
     .delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour);
 
 
