4a706436fd8241f89dfd829223f1348776f237fe
Vivek Negi <viveknegi230384@gmail.com>
2026-01-14 20:31:31 +0530

    180. Extending Our Base Template with Blocks

--- 12. natours/app.js
+++ 12. natours/app.js
@@ -71,6 +71,19 @@ app.get('/', (req, res) => {
         user: 'Jonas'
     });
 });
+
+app.get('/overview', (req, res) => {
+    res.status(200).render('overview',{
+        title: 'All Tours'
+    });
+});
+
+app.get('/tour', (req, res) => {
+    res.status(200).render('tour',{
+        title: 'The Forest Hiker Tour'
+    });
+});
+
 app.use('/api/v1/tours', tourRouter); //for this routes we want to apply this tourRouter middleware
 app.use('/api/v1/users', userRouter);
 //here above tourRouter and userRouter are the two middleware which are then mount through app.use
