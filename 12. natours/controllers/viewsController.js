73345d7c7efc51b3842adc1da31463d0fb120511
Vivek Negi <viveknegi230384@gmail.com>
2026-01-17 23:53:26 +0530

    188. Building the Login Screen

--- 12. natours/controllers/viewsController.js
+++ 12. natours/controllers/viewsController.js
@@ -1,7 +1,7 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 
-exports.getOverview = catchAsync(async(req, res) => {
+exports.getOverview = catchAsync(async(req, res,next) => {
     //1) Get tour data from collection
     const tours = await Tour.find(); 
 
@@ -16,7 +16,7 @@ exports.getOverview = catchAsync(async(req, res) => {
     });
 });
 
-exports.getTour = catchAsync(async(req, res) => {
+exports.getTour = catchAsync(async(req, res,next) => {
     //1) Get the data for the requested tour (including reviews and guides)
     const tour = await Tour.findOne({slug: req.params.slug}).populate({
       path: 'reviews',
@@ -31,3 +31,9 @@ exports.getTour = catchAsync(async(req, res) => {
         tour
     });
 });
+
+exports.getLoginForm = (req, res) => {
+    res.status(200).render('login',{
+        title: 'Log into your account'
+    });
+}
