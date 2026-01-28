8e9e9105e9809135e52bd90db666ead5e83534a9
Vivek Negi <viveknegi230384@gmail.com>
2026-01-28 22:52:00 +0530

    204. Uploading Multiple Images: Tours

--- 13. natours/controllers/tourController.js
+++ 13. natours/controllers/tourController.js
@@ -1,11 +1,38 @@
 //in this we handling of req will be done , saare exports.___ called as handlerfunction
 
 //const fs = require('fs');
+const multer = require('multer');
+const sharp = require('sharp');
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 const AppError = require('../utils/appError');
 
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith('image')) {
+        cb(null, true);
+    } else {
+        cb(new AppError('Not an image! Please upload only images.', 400), false);
+    }
+};
+
+const upload = multer({
+    storage: multerStorage,
+    fileFilter: multerFilter
+});
+
+exports.uploadTourImages = upload.fields([
+    { name: 'imageCover', maxCount: 1 },
+    { name: 'images', maxCount: 3 }
+]);
+
+exports.resizeTourImages =  (req, res, next) => {
+  console.log(req.files);
+  next();
+};
+    
 exports.aliasTopTour = (req, res, next) => {
   req.query.limit = '5';
   req.query.sort = '-ratingsAverage,price';
