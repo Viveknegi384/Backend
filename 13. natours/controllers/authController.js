80bbb031068542e090a211a4c19a2ffcbacc20b7
Vivek Negi <viveknegi230384@gmail.com>
2026-02-05 01:39:34 +0530

    207. 208. Email template with pug: sending Welcome
    and
    Sending password Reset email

--- 13. natours/controllers/authController.js
+++ 13. natours/controllers/authController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const appError = require('../utils/appError');
-const sendEmail = require('../utils/email');
+const Email = require('../utils/email');
 
 const signToken = id => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -48,6 +48,11 @@ exports.signup = catchAsync(async (req, res, next) => {
     //     role: req.body.role
     // });
     const newUser = await User.create(req.body);
+
+    const url = `${req.protocol}://${req.get('host')}/me`;
+    console.log(url);
+    await new Email(newUser, url).sendWelcome();
+
     createSendToken(newUser, 201, res);
 
 });
@@ -175,11 +180,12 @@ exports.forgetPassword = catchAsync(async (req, res, next) => {
     const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
 
     try {
-        await sendEmail({
-            email: user.email,
-            subject: 'Your password reset token (valid for 10 min)',
-            message
-        });
+        // await Email({
+        //     email: user.email,
+        //     subject: 'Your password reset token (valid for 10 min)',
+        //     message
+        // });
+        await new Email(user, resetURL).sendPasswordReset();
 
         res.status(200).json({
             status: 'success',
--- 13. natours/utils/email.js
+++ 13. natours/utils/email.js
@@ -29,7 +29,7 @@ module.exports = class Email {
   async send(template, subject) {
     //1) Render HTML based on a pug template
     const html = pug.renderFile(
-      `${__dirname}/../views/emails/${template}.pug`,
+      `${__dirname}/../views/email/${template}.pug`,
       {
         firstName: this.firstName,
         url: this.url,
@@ -54,4 +54,12 @@ module.exports = class Email {
   async sendWelcome() {
     await this.send('welcome', 'Welcome to the Natours Family!');
   }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (valid for only 10 minutes)',
+    );
+  }
+
 };
