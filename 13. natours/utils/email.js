20e642a8d8b847659c60a05e18e96aeb083b2a90
Vivek Negi <viveknegi230384@gmail.com>
2026-02-05 20:10:33 +0530

    209. using Brevo for real Email

--- 13. natours/utils/email.js
+++ 13. natours/utils/email.js
@@ -11,8 +11,15 @@ module.exports = class Email {
 
   newTransport() {
     if (process.env.NODE_ENV === 'production') {
-      //Sendgrid
-      return 1;
+      //brevo
+      return nodemailer.createTransport({
+        host: process.env.BREVO_HOST,
+        port: process.env.BREVO_PORT,
+        auth: {
+          user: process.env.BREVO_LOGIN,
+          pass: process.env.BREVO_PASSWORD,
+        },
+      });
     }
 
     return nodemailer.createTransport({
