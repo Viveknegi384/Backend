caa0ff3540e1afaa35e7363d9c44f9ce3a2d7270
Vivek Negi <viveknegi230384@gmail.com>
2026-02-06 21:51:38 +0530

    211. Integrating Stripe into the Backend

--- 13. natours/app.js
+++ 13. natours/app.js
@@ -13,6 +13,7 @@ const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
+const bookingRouter = require("./routes/bookingRoutes");
 const viewRouter = require("./routes/viewRoutes");
 
 const app = express();
@@ -109,7 +110,7 @@ app.use('/api/v1/tours', tourRouter); //for this routes we want to apply this to
 app.use('/api/v1/users', userRouter);
 //here above tourRouter and userRouter are the two middleware which are then mount through app.use
 app.use('/api/v1/reviews', reviewRouter);
-
+app.use('/api/v1/bookings', bookingRouter);
 
 app.all('*', (req, res, next) => {
     // res.status(404).json({
