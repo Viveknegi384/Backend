d944b2415064e4e640d97db53f97d72db3d3f78b
Vivek Negi <viveknegi230384@gmail.com>
2026-02-08 00:38:42 +0530

    215. Render a User's Backend Tours

--- 13. natours/routes/viewRoutes.js
+++ 13. natours/routes/viewRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const viewsController = require('../controllers/viewsController');  
+const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
 const bookingController = require('../controllers/bookingController');
 
@@ -12,12 +12,22 @@ const router = express.Router();
 //     });
 // });
 
+router.get(
+  '/',
+  bookingController.createBookingCheckout,
+  authController.isLoggedIn,
+  viewsController.getOverview,
+);
 
-router.get('/',bookingController.createBookingCheckout,authController.isLoggedIn, viewsController.getOverview);
-router.get('/tour/:slug', authController.isLoggedIn , viewsController.getTour);
-router.get('/login', authController.isLoggedIn,viewsController.getLoginForm);
+router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
+router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
+router.get('/my-tours', authController.protect, viewsController.getMyTours);
 
-router.post('/submit-user-data', authController.protect, viewsController.updateUserData);
+router.post(
+  '/submit-user-data',
+  authController.protect,
+  viewsController.updateUserData,
+);
 
 module.exports = router;
